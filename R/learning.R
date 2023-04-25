a3fe3fd7cb067057851bcd5189841e58694cca0f
Ida Kaad Faurschou <ikfaur@gmail.com>
2023-04-25 15:42:23 +0200

    commit 4

---
+++ R/learning.R
@@ -0,0 +1,19 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+1 + 1
+
+mean(2:6)
+mean(2:7)
+mean(2:7)
+mean(2:8)
+
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
